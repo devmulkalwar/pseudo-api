9162042d8d2598e484fb78c2d0db3aa346417bb5
devmulkalwar <devmulkalwar95@gmail.com>
2025-03-01 17:24:06 +0530

    backend structure created

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,13 @@
-import express from  "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
-
 app.use(cors());
 
-app.get("/data", (req, res) => {
-    res.json({ message: "Hello, World!" });
-    console.log("Received GET request at /data");
-    // Simulate a delay of 1 second
-    setTimeout(() => {
-        console.log("Data processing completed");
-    }, 1000);
+app.get('/api', (req, res) => {
+    res.send("hello world ");
 });
 
-const PORT =3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, (err, res) => {
+    console.log("server is running on port 3000");
 });
